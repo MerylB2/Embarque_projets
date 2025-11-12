0b7fba5e5dff54d654104d11e4277955d97f1f8e
cmetee-b <claudinebitee@gmail.com>
2025-11-12 13:12:33 +0100

    m06 update

---
+++ Module06/M06/ex00/i2c.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   i2c.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmetee-b <cmetee-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 13:06:53 by cmetee-b          #+#    #+#             */
+/*   Updated: 2025/11/12 13:11:31 by cmetee-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+/* Module I2C (Inter-Integrated Circuit)
+ * Soit TWI (Two Wire Interface) pour l'ATmega328p
+ * 2 fils :
+ *      SDA (Serial Data): ligne de données bidirectionnelle
+ *      SCL (Serial Clock) : ligne d'horloge générée par le maître
+ *   
+ */
+
+void   i2c_init(void)
+{
+    
+}
+
+void    i2c_start(void)
+{
+    
+}
+
+void    i2c_stop(void)
+{
+    
+}
